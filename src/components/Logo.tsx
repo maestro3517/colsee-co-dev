5890ac48e70a210d7ccb20ce916ca673a4007c70
maestro3517 <mrtzk21@gmail.com>
2025-03-28 22:55:31 +0530

    update Logo component dimensions for improved visibility

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,8 +7,8 @@ const Logo: React.FC = () => {
       <Image
         src="https://assets.co.dev/96f77038-8ef2-4350-be52-4912f83b3206/colsee-logo-6287e90.png"
         alt="Colsee Paints Logo"
-        width={150}
-        height={45}
+        width={250}
+        height={85}
         priority
         className="object-contain"
       />
